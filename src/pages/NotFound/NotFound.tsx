f0f4e16c8a5502db013e0d3f81eff0e59dfc95c2
Mario Gonzalez Duarte <magodu.pral@gmail.com>
2023-11-15 20:59:49 +0100

    not found page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,23 +1,40 @@
 import { useTranslation } from 'react-i18next';
+import { useNavigate, useSearchParams, Link } from 'react-router-dom';
 
 import classes from './NotFound.module.scss';
 
+import notFoundImg from '../../assets/images/404-image.png';
+
 const NotFound = () => {
     const { t } = useTranslation();
+    const navigate = useNavigate();
+
+    const goHome = () => {
+        navigate('/home');
+    };
 
     return (
-        <div className={classes.container}>
-            <div className={`${classes.text} row`} >
-                <div className="col-md-12 text-center">
-                    <div className="mb-4 lead">{t('common.notFoundMsg')}</div>
+ 
+        <section className={classes['not_found-section']}>     
+            <div className={classes['image-wrapper']}>
+                <div className={classes.image}>
+                    <img src={notFoundImg} alt="" />
                 </div>
             </div>
             <div className={`${classes.error} col-md-12`} >
                 <div className={classes['error-text']}>
-                    <h4>error</h4>
+                    <h4>{t('common.notPageFoundMsg')}</h4>
                 </div>
             </div>
-        </div>
+            <div className={classes.text}>
+                {t('common.notFoundGoBackMsg')}
+            </div>
+            <div className={classes.buttons}>
+                <button type="button" className={classes.large} disabled={false} onClick={() => goHome()}>
+                    Back Home
+                </button>
+            </div>
+        </section>
     );
 };
 
